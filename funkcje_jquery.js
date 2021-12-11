c9ebc96ae44c32d053410d2d6bbb7e16ce32436c
BlankaWojcik <blanka1705@wp.pl>
2021-12-11 21:00:08 +0100

    master

--- funkcje_jquery.js
+++ funkcje_jquery.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
     $("#button_01").click(function() {
       $("p").hide();
     });
+
+    $("#button_02").click(function() {
+        $("#p_03").hide();
+      });
+
 });
--- selectors01.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    $("#button_02").click(function() {
-      $("#p_03").hide();
-    });
-});
