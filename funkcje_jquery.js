aaab63ac1441f0e79aada2c11df002c150039ccd
BlankaWojcik <blanka1705@wp.pl>
2021-12-11 22:04:06 +0100

    master

--- funkcje_jquery.js
+++ funkcje_jquery.js
@@ -2,15 +2,22 @@ $(document).ready(function() {
     $("#button_01").click(function() {
       $("p").hide();
     });
-
     $("#button_02").click(function() {
         $("#p_03").hide();
       });
 
-    $("#button_03").click(function() {
-        $(".cp_01").hide();
+
+    $("#button_001").click(function() {
+       $("ul li:first");
       });
-
-
+    $("#button_002").click(function() {
+        $(":button");
+      });
+    $("#button_003").click(function() {
+        $("li:even");
+      });
+     $("#button_004").click(function() {
+        $("li:odd") ;
+      });
 
 });
