20460e27e9c082399b394d47e3123d6fcb37ea0c
BlankaWojcik <blanka1705@wp.pl>
2021-12-12 01:26:08 +0100

    master

--- jquery_effects.js
+++ jquery_effects.js
@@ -15,4 +15,14 @@ $(document).ready(function() {
         $("div").fadeTo(0.6);
     });  
 
+    $("#button_1").click(function() {
+        $("p").slideDown();
+    }); 
+    $("#button_2").click(function() {
+        $("p").slideUp();
+    }); 
+    $("#button_3").click(function() {
+        $("p").slideToggle();
+    }); 
+
 });
